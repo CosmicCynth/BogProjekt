763b82e8efe6b9e70d4ee1c41b4f2094bee46c2b
CosmicCynth <theshinyseal@gmail.com>
2025-02-04 09:14:36 +0100

    Program

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def start():
     print("Halløj med dig, vil registrere en bog eller læse en?")
     brugersvar = input("")
     brugersvar = brugersvar.upper()
-    if brugersvar == "REGISTRER":
+    if brugersvar == "REGISTRER" or "REG":
         registrereEnBog()
     elif brugersvar == "LÆSE":
         læse()
@@ -17,8 +17,17 @@ def læse():
 def registrereEnBog():
     title = input("Hvad er navnet på bogen?")
     title = str(title)
-    print("Bekræft at du sikker med ja eller nej! Titlen er " + title)
+    bekræftelseAfTitle = input("Bekræft at du sikker med ja eller nej! Titlen er " + title)
+    bekræftelseAfTitle = bekræftelseAfTitle.upper()
+    if bekræftelseAfTitle == "JA":
+        registrereEnBogGenre()
+    else:
+        registrereEnBog()
 
+
+
+def registrereEnBogGenre():
+    print("erkgpoerogore")
 
 
 
