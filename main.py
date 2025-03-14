c5d4f82d3d879d0a77727d8ba08df72bdc137c16
CosmicCynth <theshinyseal@gmail.com>
2025-03-14 12:53:45 +0100

    Program

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+bøger = {}
+bogID = 1
+
 def start():
-    print("Halløj med dig, vil registrere en bog eller læse en?")
-    brugersvar = input("")
+    brugersvar = input("Halløj med dig, vil registrere en bog eller læse en?")
     brugersvar = brugersvar.upper()
     if brugersvar == "REGISTRER" or "REG":
         registrereEnBog()
@@ -14,22 +16,60 @@ def start():
 def læse():
     print("Læse")
 
+
+
 def registrereEnBog():
-    title = input("Hvad er navnet på bogen?")
+    title = input("Hvad er navnet på bogen? ")
     title = str(title)
-    bekræftelseAfTitle = input("Bekræft at du sikker med ja eller nej! Titlen er " + title)
-    bekræftelseAfTitle = bekræftelseAfTitle.upper()
-    if bekræftelseAfTitle == "JA":
+    bekræftelse = input("Bekræft at du sikker med ja eller nej! Titlen er " + title + " ")
+    bekræftelse = bekræftelse.upper()
+    if bekræftelse == "JA":
+        bøger[bogID] = {} # Her laver vi et nested dict!
+        bøger[bogID]["Titel"] = title
         registrereEnBogGenre()
+
     else:
+        bekræftelse = ""
         registrereEnBog()
 
-
-
 def registrereEnBogGenre():
-    print("erkgpoerogore")
+    genre = input("Hvilken genre er bogen? ")
+    bekræftelse = input("Bekræft at du sikker med ja eller nej! Genren er " + genre + " ")
+    bekræftelse = bekræftelse.upper()
+    if bekræftelse == "JA":
+        bøger[bogID]["Genre"] = genre
+        registrereEnBogForfatter()
+    else:
+        registrereEnBogGenre()
+        bekræftelse = ""
+
+
+def registrereEnBogForfatter():
+    forfatter = input("Hvad hedder forfatteren? ")
+    bekræftelse = input("Bekræft at du sikker med ja eller nej! Forfatteren er " + forfatter + " ")
+    bekræftelse = bekræftelse.upper()
+    if bekræftelse == "JA":
+        bøger[bogID]["Forfatter"] = forfatter
+        registrereEnBogISBN()
+    else:
+        registrereEnBogForfatter()
+
+
+def registrereEnBogISBN():
+    global bogID
+    ISBN = input("Hvad er ISBN koden? (Den skal være 13 cifre) ")
+    ISBN = str(ISBN)
+    if len(ISBN) == 13:
+        bøger[bogID]["ISBN"] = ISBN
+        bøger[bogID]["status"] = "ulæst"
+        print("korrekt din bog er nu indlæst!")
+        bogID += 1
+        print(bøger)
+        start()
+    else:
+        print("FORKERT")
+        registrereEnBogISBN()
 
 
 
 start()
-
